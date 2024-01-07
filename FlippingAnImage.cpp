15382341f3767e253c0149d125c554ddb6103e1b
gresadaqi <155890493+gresadaqi@users.noreply.github.com>
2024-01-08 00:22:20 +0100

    Add files via upload

---
+++ FlippingAnImage.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
+        int n = image.size();
+
+       
+        for (int i = 0; i < n; ++i) {
+            reverse(image[i].begin(), image[i].end());
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                image[i][j] = 1 - image[i][j];
+            }
+        }
+
+        return image;
+    }
+};
+
+int main() {
+    //Shembull
+    vector<vector<int>> image = { {1, 0, 1}, {0, 1, 0}, {1, 1, 1} };
+
+    Solution sol;
+    vector<vector<int>> result = sol.flipAndInvertImage(image);
+    for (const auto& row : result) {
+        for (int pixel : row) {
+            cout << pixel << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
